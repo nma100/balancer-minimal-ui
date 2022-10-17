56bfd7f54b85a70321e2df0669abf1111bfe63ee
Nma <fatchoumakou@free.fr>
2022-10-17 21:30:23 +0200

    Bnum to string

--- src/utils/bnum.js
+++ src/utils/bnum.js
@@ -3,15 +3,12 @@ import BigNumber from 'bignumber.js';
 
 export const ZERO = new BigNumber(0);
 
-export function bnum(val)  {
+export function bnum(val) {
     if (!val) return ZERO;
     return new BigNumber(val);
 }
 
-export function bnumToStr(val, precision = 2)  {
-    if (BigNumber.isBigNumber(val)) {
-        return val.toFixed(precision)
-    } else { 
-        return bnumToStr(ZERO, precision);
-    }
+export function bnumToStr(val, precision = 2) {
+    const _val = BigNumber.isBigNumber(val) ? val : ZERO;
+    return _val.toFixed(precision)
 }
